9d278947aefae78e6f2adab08c0b78a763f6a13f
Wim Haeck <whaeck@gmail.com>
2020-02-19 16:11:11 -0700

    Updated to use standard C++17 optional and variant

--- src/interpolation.hpp
+++ src/interpolation.hpp
@@ -4,24 +4,8 @@
 #include <cmath>
 #include <type_traits>
 
-#ifdef USING_CPP17
-  #include <optional>
-  #include <variant>
-#else 
-  #ifdef __GNUC__
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wunused-parameter"
-  #endif
-  #include <variant>	
-  #ifdef __GNUC__
-    #pragma GCC diagnostic pop
-  #endif
-  #include <experimental/optional>
-
-namespace std {
-    using namespace std::experimental;
-  }
-#endif
+#include <optional>
+#include <variant>
 
 #include "Log.hpp"
 #include "boost/hana.hpp"
