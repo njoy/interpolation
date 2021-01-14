36bab5817cae7f4a26ab2e5635bd6edcdcd8bf88
Hunter Belanger <hunter.belanger@gmail.com>
2021-01-14 17:31:51 +0100

    adds integration to LinearLinear

--- src/interpolation/Interpolant/LinearLinear/test/LinearLinear.test.cpp
+++ src/interpolation/Interpolant/LinearLinear/test/LinearLinear.test.cpp
@@ -30,6 +30,18 @@ SCENARIO("The LinearLinear interpolant computes the correct inversion",
   REQUIRE( xRight == LinearLinear::invert( yRight, xLeft, xRight, yLeft, yRight ) );
 }
 
+SCENARIO("The LinearLinear interpolant computes the correct integral",
+         "[interpolant], [LinearLinear]"){
+  double xLeft = 0.0, xRight = 1.0;
+  auto f = []( double x ){ return 10.0 * x + 10; };
+  double yLeft = f( xLeft ), yRight = f( xRight );
+  auto F = [](double x){ return 5.*(x*x) + (10.*x); };
+  
+  REQUIRE( F(0.5) == LinearLinear::integrate( 0., 0.5, xLeft, xRight, yLeft, yRight ) );
+  REQUIRE( F(0.75) == LinearLinear::integrate( 0., 0.75, xLeft, xRight, yLeft, yRight ) );
+  REQUIRE( F(0.75) - F(0.25) == LinearLinear::integrate( 0.25, 0.75, xLeft, xRight, yLeft, yRight ) );
+}
+
 SCENARIO("The LinearLinear interpolant is compatible with units",
          "[interpolant], [Histogram]"){
   auto xLeft = 0.0 * electronVolts;
@@ -40,4 +52,8 @@ SCENARIO("The LinearLinear interpolant is compatible with units",
 	   LinearLinear::apply( 0.5 * electronVolts, xLeft, xRight, yLeft, yRight ) );
   REQUIRE( 0.5 * electronVolts ==
 	   LinearLinear::invert( 15.0 * barns, xLeft, xRight, yLeft, yRight ) );  
+  auto xLow = 0. * electronVolts;
+  auto xHi  = 1. * electronVolts;
+  REQUIRE(15.*electronVolts*barns ==
+          LinearLinear::integrate(xLow, xHi, xLeft, xRight, yLeft, yRight));
 }
