abc97df689c4aedaee7213b5349661de0e06ba59
Hunter Belanger <hunter.belanger@gmail.com>
2021-01-14 19:32:05 +0100

    move LinearLogarithmic tests to their own file

--- src/interpolation/Interpolant/LinearLogarithmic/test/integration.test.cpp
+++ src/interpolation/Interpolant/LinearLogarithmic/test/integration.test.cpp
@@ -8,11 +8,13 @@
 using namespace njoy::interpolation;
 using namespace dimwits;
 
-SCENARIO("The LinearLogarithmic interpolant computes the correct value",
+SCENARIO("The LinearLogarithmic interpolant computes the correct integral",
          "[interpolant], [LinearLogarithmic]"){
   std::vector<double> xValues{ 1E-2, 1, 10 };
   auto f1 = []( double x ){ return 13.2 * std::log(3.2*x); };
   auto f2 = []( double x ){ return -5.6 * std::log(2.4*x); };
+  auto F1 = [](double x){return 13.2*x*(std::log(3.2*x)-1.);};
+  auto F2 = [](double x){return -5.6*x*(std::log(2.4*x)-1.);};
 
   auto avg =
     []( double xLeft, double xRight ){ return 0.5 * ( xLeft + xRight ); };
@@ -30,24 +32,25 @@ SCENARIO("The LinearLogarithmic interpolant computes the correct value",
     double y1Left = f1( xLeft ), y1Right = f1( xRight );
     double y2Left = f2( xLeft ), y2Right = f2( xRight );
     double xBar = avg( xLeft, xRight );
+    double xLow = xLeft, xHi = xRight;
 
-    REQUIRE( not excessiveError( y1Left,
-                                 LinearLogarithmic::apply
-                                 ( xLeft, xLeft, xRight, y1Left, y1Right ) ) );
-    REQUIRE( not excessiveError( y2Left,
-                                 LinearLogarithmic::apply
-                                 ( xLeft, xLeft, xRight, y2Left, y2Right ) ) );
+    REQUIRE( not excessiveError( F1(xBar) - F1(xLow),
+                                 LinearLogarithmic::integrate
+                                 ( xLow, xBar, xLeft, xRight, y1Left, y1Right ) ) );
+    REQUIRE( not excessiveError( F2(xBar) - F2(xLow),
+                                 LinearLogarithmic::integrate
+                                 ( xLow, xBar, xLeft, xRight, y2Left, y2Right ) ) );
 
-    REQUIRE( not excessiveError( f1( xBar ),
-                                 LinearLogarithmic::apply
-                                 ( xBar, xLeft, xRight, y1Left, y1Right ) ) );
-    REQUIRE( not excessiveError( f2( xBar ),
-                                 LinearLogarithmic::apply
-                                 ( xBar, xLeft, xRight, y2Left, y2Right ) ) );
+    REQUIRE( not excessiveError( F1(xHi) - F1(xBar),
+                                 LinearLogarithmic::integrate
+                                 ( xBar, xHi, xLeft, xRight, y1Left, y1Right ) ) );
+    REQUIRE( not excessiveError( F2(xHi) - F2(xBar),
+                                 LinearLogarithmic::integrate
+                                 ( xBar, xHi, xLeft, xRight, y2Left, y2Right ) ) );
   } while( iterator != last );
 }
 
-SCENARIO("The LinearLogarithmic interpolant computes is compatible with units",
+SCENARIO("The LinearLogarithmic interpolant integration is compatible with units",
          "[interpolant], [LinearLogarithmic]"){
   std::vector< double > xValues{ 1E-2, 1, 10 };
   auto f1 = []( auto x ){ return 13.2 * std::log(3.2 * x.value) * barn; };
@@ -77,19 +80,12 @@ SCENARIO("The LinearLogarithmic interpolant computes is compatible with units",
     auto xBar = avg( xLeft, xRight );
     auto xLow = xLeft, xHi = xRight;
 
-    REQUIRE( not excessiveError( y1Left,
-                                 LinearLogarithmic::apply
-                                 ( xLeft, xLeft, xRight, y1Left, y1Right ) ) );
-    REQUIRE( not excessiveError( y2Left,
-                                 LinearLogarithmic::apply
-                                 ( xLeft, xLeft, xRight, y2Left, y2Right ) ) );
+    REQUIRE( not excessiveError( F2(xBar) - F2(xLow),
+                                 LinearLogarithmic::integrate
+                                 ( xLow, xBar, xLeft, xRight, y2Left, y2Right ) ) );
 
-    REQUIRE( not excessiveError( f1( xBar ),
-                                 LinearLogarithmic::apply
-                                 ( xBar, xLeft, xRight, y1Left, y1Right ) ) );
-    REQUIRE( not excessiveError( f2( xBar ),
-                                 LinearLogarithmic::apply
-                                 ( xBar, xLeft, xRight, y2Left, y2Right ) ) );
-
+    REQUIRE( not excessiveError( F1(xHi) - F1(xBar),
+                                 LinearLogarithmic::integrate
+                                 ( xBar, xHi, xLeft, xRight, y1Left, y1Right ) ) );
   } while( iterator != last );
 }
