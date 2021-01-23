823967162e04458c3ce9a8ec9669aaa13e3f793e
Hunter Belanger <hunter.belanger@gmail.com>
2021-01-23 17:15:41 +0100

    Adds integration for domain Min and Max decorators

--- src/interpolation/table/domain/Max.hpp
+++ src/interpolation/table/domain/Max.hpp
@@ -15,6 +15,29 @@ protected:
 					      std::forward<Args>(args)... );
   }
 
+  template< typename Table, typename Arg, typename... Args >
+  auto integrate( Arg&& xLow, Arg&& xHi, Args&&... args ) const {
+    // Captures child leftLimit method
+    const auto& table = static_cast< const Table& >( *this );
+    if ( xLow > table.domainMax() ){
+      Log::error( "Lower integration limit is greater than right domain limit." );
+      Log::info( "Lower integration limit: {}", xLow );
+      Log::info( "Right domain limit: {}", table.domainMax() );
+      throw std::out_of_range( "Lower integration limit is outside table domain." );
+    }
+
+    if ( xHi > table.domainMax() ){
+      Log::error( "Upper integration limit is greater than right domain limit." );
+      Log::info( "Uppper integration limit: {}", xHi );
+      Log::info( "Right domain limit: {}", table.domainMax() );
+      throw std::out_of_range( "Upper integration limit is outside table domain." );
+    }
+
+    return Parent:: template integrate<Parent>( std::forward<Arg>(xLow),
+                 std::forward<Arg>(xHi),
+					       std::forward<Args>(args)... );
+  }
+
 public:
   static constexpr auto specifiesDomainMax(){ return true; }
   
--- src/interpolation/table/domain/Min.hpp
+++ src/interpolation/table/domain/Min.hpp
@@ -15,6 +15,29 @@ protected:
 					       std::forward<Args>(args)... );
   }
 
+  template< typename Table, typename Arg, typename... Args >
+  auto integrate( Arg&& xLow, Arg&& xHi, Args&&... args ) const {
+    // Captures child leftLimit method
+    const auto& table = static_cast< const Table& >( *this );
+    if ( xLow < table.domainMin() ){
+      Log::error( "Lower integration limit is less than left domain limit." );
+      Log::info( "Lower integration limit: {}", xLow );
+      Log::info( "Left domain limit: {}", table.domainMin() );
+      throw std::out_of_range( "Lower integration limit is outside table domain." );
+    }
+
+    if ( xHi < table.domainMin() ){
+      Log::error( "Upper integration limit is less than left domain limit." );
+      Log::info( "Uppper integration limit: {}", xHi );
+      Log::info( "Left domain limit: {}", table.domainMin() );
+      throw std::out_of_range( "Upper integration limit is outside table domain." );
+    }
+
+    return Parent:: template integrate<Parent>( std::forward<Arg>(xLow),
+                 std::forward<Arg>(xHi),
+					       std::forward<Args>(args)... );
+  }
+
 public:
   static constexpr auto specifiesDomainMin(){ return true; }
   
